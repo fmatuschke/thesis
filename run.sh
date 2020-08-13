52e930936de318257ca81904fe3d818703a67568
fmatuschke <f.matuschke@fz-juelich.de>
2020-08-13 08:39:59 +0200

    RFC run file

--- run.sh
+++ run.sh
@@ -6,12 +6,18 @@ if [ "$1" = "--clean" ]; then
    rm -r output
 fi
 
+if [ "$1" = "--single" ]; then
+   lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/ -shell-escape thesis.tex
+   xdg-open output/thesis.pdf &> /dev/null 2>&1
+   exit
+fi
+
 mkdir -p output/tikz/output/tikz
 find . -type d -not -path "./output*" -exec mkdir -p output/{} \;
 
 #find . -type f -exec sed -i.bak '/^%/!s/\([.!?]\) \([[:upper:]]\)/\1\n\2/g' {} \;
 
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/ --shell-escape thesis.tex
+lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/ -shell-escape thesis.tex
 biber output/thesis
-lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/ --shell-escape thesis.tex
-xdg-open output/thesis.pdf
+lualatex -interaction=nonstopmode -halt-on-error -output-directory=output/ -shell-escape thesis.tex
+xdg-open output/thesis.pdf &> /dev/null 2>&1
